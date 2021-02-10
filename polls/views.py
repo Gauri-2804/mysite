ddf8db6df1605687b83aad3c4464b6dc261e7b3a
GAURI <gauripawale2804@gmail.com>
2021-02-10 18:34:52 +0530

    Added Template

--- polls/views.py
+++ polls/views.py
@@ -10,17 +10,3 @@ def index(request):
     return HttpResponse("Hello, World. You're at the polls index.")
     #return render(request, 'polls/home.html')
 
-'''
-
-from django.shortcuts import render  
-
-#importing loading from django template  
-from django.template import loader  
-    
-# Create your views here.  
-from django.http import HttpResponse  
-def index(request):  
-    template = loader.get_template('home.html') # getting our template  
-    return HttpResponse(template.render())       # rendering the template in HttpResponse  
-
-'''
